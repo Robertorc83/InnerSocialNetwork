6a2d3bb1b5422f11c5556d22a78dce77334f3898
Robertorc83 <50055949+Robertorc83@users.noreply.github.com>
2022-06-16 17:26:38 -0500

    setup and creation of signup and login forms

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'username' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only(['email', 'password']));
+
+        return redirect()->route('dashboard');
+    }
+    
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <div class="flex justify-center ">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            <h1 class="text-3xl font-bold">Dashboard</h1>
+</div>
+@endsection
